ed323b5ff6fd76af7ceb78fd19d4c530cdc8149b
IlyaMuravjov <muravjovilya@gmail.com>
2020-12-09 17:06:31 +0300

    Add output file argument to word_counter, replace rewind with fseek

--- src/examples/word_counter.c
+++ src/examples/word_counter.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <inttypes.h>
-#include <string.h>
 #include <ctype.h>
 #include <time.h>
 #include <stdlib.h>
@@ -10,10 +9,11 @@
 
 #define MAX_WORD_LENGTH 1024
 
-static const int MIN_ARG_COUNT = 1 + 3;
+static const int MIN_ARG_COUNT = 1 + 4;
 static const int INPUT_FILE_ARG_INDEX = 1;
-static const int LOAD_FACTOR_ARG_INDEX = 2;
-static const int FIRST_CAPACITY_ARG_INDEX = 3;
+static const int OUTPUT_FILE_ARG_INDEX = 2;
+static const int LOAD_FACTOR_ARG_INDEX = 3;
+static const int FIRST_CAPACITY_ARG_INDEX = 4;
 
 int str_to_size_t(char *str, size_t *result, const char *variable_name) {
     char *endptr;
@@ -87,10 +87,10 @@ ENTRY *get_most_common_word_entry(MAP *word_to_count_map) {
     return most_common_word_entry;
 }
 
-int run_experiment(FILE *input_file, size_t init_capacity, float load_factor) {
+int run_experiment(FILE *input_file, FILE *output_file, size_t init_capacity, float load_factor) {
     clock_t clock_before = clock();
-    if (printf("\nInitial word_to_count_map capacity: %zu\n", init_capacity) < 0) {
-        LOG_ERROR("Unable to print initial word_to_count_map capacity\n");
+    if (fprintf(output_file, "\nInitial capacity: %zu\n", init_capacity) < 0) {
+        LOG_ERROR("Unable to print initial capacity\n");
         return -1;
     }
     MAP *word_to_count_map = get_word_to_count_map(input_file, init_capacity, load_factor);
@@ -100,9 +100,9 @@ int run_experiment(FILE *input_file, size_t init_capacity, float load_factor) {
         MAP_free(word_to_count_map);
         return -1;
     }
-    MAP_fprint_stats(word_to_count_map, stdout);
+    MAP_fprint_stats(word_to_count_map, output_file);
     if (MAP_log_and_free_on_error(word_to_count_map)) return -1;
-    if (printf("Unique word count: %zu\n"
+    if (fprintf(output_file, "Unique word count: %zu\n"
                "Most common word: %s (appears %d times)\n",
                MAP_size(word_to_count_map), ENTRY_get_key(most_common_word_entry),
                ENTRY_get_value(most_common_word_entry)) < 0) {
@@ -112,7 +112,7 @@ int run_experiment(FILE *input_file, size_t init_capacity, float load_factor) {
     }
     MAP_free(word_to_count_map);
     clock_t clock_after = clock();
-    if (printf("Time taken: %f seconds\n\n", ((double) (clock_after - clock_before)) / CLOCKS_PER_SEC) < 0) {
+    if (fprintf(output_file, "Time taken: %f seconds\n\n", ((double) (clock_after - clock_before)) / CLOCKS_PER_SEC) < 0) {
         LOG_ERROR("Unable to print execution time\n");
         return -1;
     }
@@ -122,7 +122,7 @@ int run_experiment(FILE *input_file, size_t init_capacity, float load_factor) {
 int main(int argc, char *argv[]) {
     if (argc < MIN_ARG_COUNT) {
         LOG_ERROR("Invalid number of command line arguments. Expected at least %d arg(s), but found %d arg(s)\n"
-                  "Usage: word_counter <input file> <load factor> <capacities...>\n",
+                  "Usage: word_counter <input file> <output file> <load factor> <capacities...>\n",
                   MIN_ARG_COUNT - 1, argc - 1);
         return -1;
     }
@@ -131,18 +131,34 @@ int main(int argc, char *argv[]) {
         return -1;
     FILE *input_file = fopen(argv[INPUT_FILE_ARG_INDEX], "r");
     if (input_file == NULL) {
-        LOG_ERROR("Unable to find input file \"%s\"\n", argv[INPUT_FILE_ARG_INDEX]);
+        LOG_ERROR("Unable to open input file \"%s\"\n", argv[INPUT_FILE_ARG_INDEX]);
+        return -1;
+    }
+    FILE *output_file = fopen(argv[OUTPUT_FILE_ARG_INDEX], "w");
+    if (input_file == NULL) {
+        LOG_ERROR("Unable to open output file \"%s\"\n", argv[OUTPUT_FILE_ARG_INDEX]);
+        fclose(input_file);
         return -1;
     }
     size_t capacity;
     for (int i = FIRST_CAPACITY_ARG_INDEX; i < argc; i++) {
         if (str_to_size_t(argv[i], &capacity, "capacity") != 0
-            || run_experiment(input_file, capacity, load_factor) != 0) {
+            || run_experiment(input_file, output_file, capacity, load_factor) != 0) {
             fclose(input_file);
+            fclose(output_file);
             return -1;
         }
-        rewind(input_file);
+        if(fseek(input_file, 0, SEEK_SET) != 0) {
+            LOG_ERROR("Unable to rewind input file \"%s\"\n", argv[INPUT_FILE_ARG_INDEX]);
+            fclose(input_file);
+            fclose(output_file);
+            return -1;
+        }
     }
     fclose(input_file);
+    if(fclose(output_file) != 0) {
+        LOG_ERROR("Unable to close output file\n");
+        return -1;
+    }
     return 0;
 }
